99aa8626cb0b311835fa3ef533306477b4aeb12a
Dayvson Spacca <spacca.dayvson@gmail.com>
2021-01-18 08:45:09 -0300

    Updated queue

--- estuturade-dado-fila/fila.py
+++ estuturade-dado-fila/fila.py
@@ -12,7 +12,6 @@ class Queue:
 
     def __init__(self):
         self.queue = []
-        self.lenght = len(self.queue)
 
     def dequeue(self):
         return self.queue.pop(0)
@@ -23,11 +22,22 @@ class Queue:
     def show(self):
         print(self.queue)
 
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        if self.index < len(self.queue):
+            item = self.queue[self.index]
+            self.index += 1
+            return item
+        else:
+            raise StopIteration
+
 
 def main():
 
     queue = Queue()
-
     # Inserção de valores aleatórios na fila
     for i in range(5):
         queue.enqueue(randint(0, 10))
@@ -39,6 +49,10 @@ def main():
     queue.dequeue()
     # Consultando novamente a fila.
     queue.show()
+
+    # Iterando na fila.
+    for item in queue:
+        print(item)
 
 
 if __name__ == "__main__":
